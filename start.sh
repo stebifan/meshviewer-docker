4d068f983fe8d531d673832d742e1369d151c380
stebifan <stefan@tro-mail.de>
2018-04-10 22:22:34 +0200

    bugfix

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-cd /opt/meshviewer
 git clone https://github.com/Freifunk-Troisdorf/meshviewer.git /opt/meshviewer/
+cd /opt/meshviewer
 npm install gulp -D yarn 
 yarn global add gulp-cli
 gulp
 ln -s /opt/meshviewer/build/index.html /opt/meshviewer/index.html
-exit 0
