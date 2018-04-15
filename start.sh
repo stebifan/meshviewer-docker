7491bbce987c78be18596c1b0abc10a3a266c1d0
stebifan <stefan@tro-mail.de>
2018-04-15 11:03:01 +0200

    test

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 git clone https://github.com/Freifunk-Troisdorf/meshviewer.git /opt/tmp
 cd /opt/tmp
-npm install gulp -D yarn 
+npm install gulp -D
+yarn
 yarn global add gulp-cli
 gulp
 cp -r /opt/tmp/build/* /opt/meshviewer/build/
